8632f699a4a8b3e1c03144a1f037507ee5903718
Epulvis <zuhrisaifuddin405@gmail.com>
2024-11-21 11:13:23 +0700

    build

---
+++ TugasBesarDPBO/src/main/java/com/mycompany/tugasbesardpbo/TugasBesarDPBO.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tugasbesardpbo;
+
+/**
+ *
+ * @author Zuhri
+ */
+public class TugasBesarDPBO {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
