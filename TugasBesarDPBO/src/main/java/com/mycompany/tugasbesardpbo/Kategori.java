ed9cd5ee01ee60b67de1a27c311b0de57a71c819
Epulvis <zuhrisaifuddin405@gmail.com>
2024-11-21 11:06:04 +0700

    update class kategori

---
+++ TugasBesarDPBO/src/main/java/com/mycompany/tugasbesardpbo/Kategori.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.tugasbesardpbo;
+
+/**
+ *
+ * @author Zuhri
+ */
+public class Kategori {
+    private String Jenis_kategori;
+    private String Isi_barang;
+    private Produk produk;
+    
+}
