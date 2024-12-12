b2c7987b0eda1cbc006499adf3c26b01443dff23
Aslam Rizky <146598910+aslamrizky@users.noreply.github.com>
2024-12-12 20:44:02 +0700

    Update Chat
    Sudah terdapat code

--- src/main/java/com/mycompany/tubes/dpbo/kel4/Chat.java
+++ src/main/java/com/mycompany/tubes/dpbo/kel4/Chat.java
@@ -4,10 +4,41 @@
  */
 package com.mycompany.tubes.dpbo.kel4;
 
-/**
- *
- * @author aslam
- */
+import java.util.ArrayList;
+
 public class Chat {
-    
+    private String idPembeli;
+    private String namaPembeli;
+    private String idPenjual;
+    private String namaPenjual;
+    private ArrayList<String> riwayatChat;
+
+    public Chat(String idPembeli, String namaPembeli, String idPenjual, String namaPenjual) {
+        this.idPembeli = idPembeli;
+        this.namaPembeli = namaPembeli;
+        this.idPenjual = idPenjual;
+        this.namaPenjual = namaPenjual;
+        this.riwayatChat = new ArrayList<>();
+    }
+
+    public void tambahChat(String pengirim, String pesan) {
+        String chatEntry = pengirim + ": " + pesan;
+        this.riwayatChat.add(chatEntry);
+        // Simpan chat ke database
+    }
+
+    public void lihatRiwayatChat() {
+        System.out.println("Riwayat Chat:");
+        for (String chat : this.riwayatChat) {
+            System.out.println(chat);
+        }
+    }
+
+    public void kirimPesanPembeli(String pesan) {
+        this.tambahChat(this.namaPembeli, pesan);
+    }
+
+    public void kirimPesanPenjual(String pesan) {
+        this.tambahChat(this.namaPenjual, pesan);
+    }
 }
